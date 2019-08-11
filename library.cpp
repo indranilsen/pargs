c75e509f6c26f554995ecef50a4b4079dae16ccf
Indranil Sen <indranilsen010@gmail.com>
2019-08-11 16:40:57 -0500

    Initial commit

---
+++ library.cpp
@@ -0,0 +1,100 @@
+#include "library.h"
+
+#include <iostream>
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+namespace pargs_n {
+    struct pair_t {
+        string key;
+        string val;
+    };
+
+    bool verify_expected_arguments(std::map<std::string, std::string>* arguments,
+                                   std::vector<std::string>* expected_arguments);
+    bool is_argument(char *long_arg);
+    pair_t get_argument(char *long_arg);
+
+    pargs::pargs(int arg_count, char **arg_value)
+            : argc(arg_count), argv(arg_value) {}
+
+    bool pargs::parse() {
+        if (argc < 2) {
+            return false;
+        }
+
+        for (int i = 0; i < argc; i++) {
+            if (is_argument(argv[i])) {
+                pair_t arg = get_argument(argv[i]);
+                arguments[arg.key] = arg.val;
+            }
+        }
+
+        return verify_expected_arguments(&arguments, &expected_arguments);
+    }
+
+    void pargs::expect(std::string argument) {
+        expected_arguments.push_back(argument);
+    }
+
+    std::string pargs::get_arg(std::string argument) {
+        if (arguments.find(argument) != arguments.end()) {
+            return arguments[argument];
+        } else {
+            return "";
+        }
+    }
+
+    std::string pargs::get_arg_or_default(std::string argument, std::string default_value) {
+        string value = pargs::get_arg(argument);
+        return value.empty() ? default_value : value;
+    }
+
+    void pargs::set_error_message(std::string error_message) {
+        err_msg = error_message;
+    }
+
+    std::string pargs::error_message() {
+        return err_msg;
+    }
+
+    bool pargs::help_asked() {
+        return arguments.find("help") != arguments.end();
+    }
+
+    void pargs::print_all_args() {
+        for (auto it = arguments.begin(); it != arguments.end(); ++it) {
+            cout << it->first << ": " << it->second << endl;
+        }
+    }
+
+    bool verify_expected_arguments(std::map<std::string, std::string>* arguments,
+            std::vector<std::string>* expected_arguments) {
+        for (auto const& expected_arg : *expected_arguments) {
+            if (arguments->find(expected_arg) == arguments->end()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool is_argument(char *long_arg) {
+        string str(long_arg);
+        return str.find("--") == 0;
+    }
+
+    pair_t get_argument(char *long_arg) {
+        string arg(long_arg);
+        unsigned long pos_eq = arg.find('=');
+        if (pos_eq != string::npos) {
+            return {arg.substr(2, pos_eq - 2), arg.substr(pos_eq + 1, arg.length())};
+        } else {
+            return {arg.substr(2, arg.length()), ""};
+        }
+    }
+}
---
+++ library.h
@@ -0,0 +1,30 @@
+#ifndef PARGS_LIBRARY_H
+#define PARGS_LIBRARY_H
+
+#include <string>
+#include <map>
+#include <vector>
+
+namespace pargs_n {
+    class pargs {
+    private:
+        int argc;
+        char **argv;
+        std::string err_msg;
+        std::vector<std::string> expected_arguments;
+        std::map<std::string, std::string> arguments;
+    public:
+        pargs(int arg_count, char **arg_value);
+
+        bool parse();
+        void expect(std::string argument);
+        std::string get_arg(std::string argument);
+        std::string get_arg_or_default(std::string argument, std::string default_value);
+        void set_error_message(std::string error_message);
+        std::string error_message();
+        bool help_asked();
+        void print_all_args();
+    };
+}
+
+#endif //PARGS_LIBRARY_H
